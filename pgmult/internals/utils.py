fixing numerical stability with psi_to_pi. adding axis arg to pi_to_psi

pgmult/internals/utils.py
@@ -134,36 +134,62 @@ def psi_to_pi(psi, axis=None):
 
     return pi
 
-def pi_to_psi(pi):
+def pi_to_psi(pi, axis=None):
     """
     Convert probability vector pi to a vector psi
     :param pi:      Length K probability vector
     :return:        Length K-1 transformed vector psi
     """
-    if pi.ndim == 1:
-        K = pi.size
-        assert np.allclose(pi.sum(), 1.0)
-        psi = np.zeros(K-1)
+    if axis is None:
+        if pi.ndim == 1:
+            K = pi.size
+            assert np.allclose(pi.sum(), 1.0)
+            pi = pi / (pi.sum() + 1e-8)
+            psi = np.zeros(K-1)
 
-        stick = 1.0
-        for k in range(K-1):
-            psi[k] = logit(pi[k] / stick)
-            stick -= pi[k]
+            stick = 1.0
+            for k in range(K-1):
+                psi[k] = logit(pi[k] / stick)
+                stick -= pi[k]
 
-        # DEBUG
-        assert np.allclose(stick, pi[-1])
-    elif pi.ndim == 2:
-        M, K = pi.shape
-        assert np.allclose(pi.sum(axis=1), 1.0)
-        psi = np.zeros((M,K-1))
+            # DEBUG
+            assert np.allclose(stick, pi[-1], atol=1e-7)
 
-        stick = np.ones(M)
-        for k in range(K-1):
-            psi[:,k] = logit(pi[:,k] / stick)
-            stick -= pi[:,k]
-        assert np.allclose(stick, pi[:,-1])
+        elif pi.ndim == 2:
+            M, K = pi.shape
+            assert np.allclose(pi.sum(axis=1), 1.0)
+            pi = pi / (pi.sum(axis=1, keepdims=True) + 1e-8)
+            psi = np.zeros((M,K-1))
+
+            stick = np.ones(M)
+            for k in range(K-1):
+                psi[:,k] = logit(pi[:,k] / stick)
+                stick -= pi[:,k]
+            assert np.allclose(stick, pi[:,-1], atol=1e-7)
+
+        else:
+            raise ValueError("psi must be 1 or 2D")
+
     else:
-        raise NotImplementedError
+        K = pi.shape[axis]
+
+        # Normalize pi with slack along specified axis
+        assert np.allclose(pi.sum(axis=axis), 1.0)
+        pi = pi / (pi.sum(axis=axis, keepdims=True) + 1e-8)
+
+        # Initialize psi and stick
+        psi  = np.zeros([pi.shape[dim] if dim != axis else K-1 for dim in range(pi.ndim)])
+        stick = np.squeeze(np.ones([pi.shape[dim] if dim != axis else 1   for dim in range(pi.ndim)]))
+
+        for k in range(K-1):
+            inds = [slice(None) if dim != axis else k for dim in range(psi.ndim)]
+            psi[inds] = logit(pi[inds] / stick)
+            stick -= pi[inds]
+
+        inds = [slice(None) if dim != axis else -1 for dim in range(psi.ndim)]
+        assert np.allclose(stick, pi[inds], atol=1e-7)
+
+    assert np.all(np.isfinite(psi))
 
     return psi
 

test/test_psi_pi.py
@@ -1,22 +1,57 @@
 import numpy as np
+np.random.seed(2)
+
 import matplotlib.pyplot as plt
 
 from pgmult.distributions import PGMultinomial
 from pgmult.internals.utils import compute_uniform_mean_psi, pi_to_psi, psi_to_pi
 
+from pybasicbayes.util.text import progprint_xrange
 
 def test_psi_pi_conversion():
-    K = 10
+    K = 10000
 
-    pi = np.ones(K) / float(K)
+    pi = np.random.dirichlet(0.1 * np.ones(K))
+    assert np.all(np.isfinite(pi))
+    # pi = np.ones(K) / float(K)
     psi = pi_to_psi(pi)
     pi2 = psi_to_pi(psi)
 
-    print "pi:  ", pi
-    print "psi: ", psi
-    print "pi2: ", pi2
+    # print "pi:  ", pi
+    # print "psi: ", psi
+    # print "pi2: ", pi2
 
     assert np.allclose(pi, pi2), "Mapping is not invertible."
+
+
+def test_psi_pi_conversion_2d():
+    K = 10000
+    M = 100
+
+    pi = np.random.dirichlet(0.1 * np.ones(K), size=M)
+    assert np.all(np.isfinite(pi))
+
+    psi_a = pi_to_psi(pi)
+    psi_b = pi_to_psi(pi, axis=1)
+    assert np.allclose(psi_a, psi_b)
+
+    pi_a = psi_to_pi(psi_a)
+    pi_b = psi_to_pi(psi_b, axis=1)
+    assert np.allclose(pi_a, pi_b, atol=1e-7)
+    assert np.allclose(pi_a, pi, atol=1e-7)
+
+
+def test_psi_pi_conversion_3d():
+    K = 10
+    M = 10
+
+    pi = np.random.dirichlet(0.1 * np.ones(K), size=(M,M))
+    assert pi.shape == (M,M,K)
+    assert np.all(np.isfinite(pi))
+
+    psi = pi_to_psi(pi, axis=2)
+    pi2 = psi_to_pi(psi, axis=2)
+    assert np.allclose(pi2, pi, atol=1e-7)
 
 def test_pgm_rvs():
     K = 10
@@ -125,8 +160,12 @@ def test_block_correlated_pgm_rvs():
     Sigma = np.kron(Sblocks,np.eye(3))
     test_correlated_pgm_rvs(Sigma)
 
-# test_psi_pi_conversion()
+for itr in progprint_xrange(1000):
+    # test_psi_pi_conversion()
+    # test_psi_pi_conversion_2d()
+    test_psi_pi_conversion_3d()
+
 # test_pgm_rvs()
 # test_chain_correlated_pgm_rvs()
-test_wishart_correlated_pgm_rvs(K=10)
+# test_wishart_correlated_pgm_rvs(K=10)
 # test_block_correlated_pgm_rvs()